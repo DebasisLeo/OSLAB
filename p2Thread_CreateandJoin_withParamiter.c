f318e79d39dc2cf2c9e1579e178e69971b0133bf
debasis <joydebashis8@gmail.com>
2025-05-20 15:04:06 +0600

    third commit

---
+++ p2Thread_CreateandJoin_withParamiter.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+struct Args
+{
+    /* data */int a,b;
+};
+
+void* sum(void* arg){
+struct Args* data=(struct Args*)arg;
+int a=data->a;
+int b=data->b;
+int *res=malloc(sizeof(int));
+*res=a+b;
+pthread_exit(res);
+}
+int main(){
+int a,b;
+scanf("%d %d",&a,&b);
+struct Args data;
+data.a=a;
+data.b=b;
+pthread_t T1;
+void* result;
+pthread_create(&T1,NULL,sum,(void*)&data);
+pthread_join(T1,&result);
+printf("this is main\n");
+printf("the sum is %d\n",*(int *)result);
+
+}
