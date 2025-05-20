039ecb9275314a9871e41d093f9553d367e4ace7
debasis <joydebashis8@gmail.com>
2025-05-20 22:50:36 +0600

    seventh commit

---
+++ readWriteCopy.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+int main(){
+    int fd1,fd2,n;
+    char buff[25];
+    fd1=open("f1.txt",O_RDONLY);
+    fd2=open("f3.txt",O_WRONLY|O_CREAT);
+    while((n=read(fd1,buff,20))!=0){
+write(fd2,buff,n);
+    }
+    
+}
